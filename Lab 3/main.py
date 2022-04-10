889c224c7f1ebced303c9b6289645bb20066e9da
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2022-04-11 03:38:42 +0600

    testing code added for l3 t1

---
+++ Lab 3/main.py
@@ -0,0 +1,33 @@
+import re
+file_name = input("Write ethe file name among the below two file name: \n1. Input1.txt \n2. Input2.txt \n")
+
+try:
+	file = open(file_name)
+except:
+	print("Wrong file name input")
+
+print("\n")
+
+RegCount = int(file.readline())
+RegIndex = []
+
+for i in range (int(RegCount)):
+	RegIndex.append(file.readline().rstrip())
+
+InputCount = int(file.readline())
+InputIndex = []
+
+for i in range (int(InputCount))
+InputIndex.append(file.readline().rstrip())
+
+c = false
+for i in range (InputCount):
+	for r in range (Rcount):
+		if re.compile(RegIndex[r]).search(InputIndex[i]):
+			print("YES,", r+1)
+			c = True
+			break
+		else:
+			c = False
+		if t == False:
+			Print("NO, 0")
